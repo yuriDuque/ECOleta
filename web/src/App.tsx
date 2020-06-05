489bc87f1f2ea587068843dae1389ba9015157a8
Yuri Duque <yuri.duque@hotmail.com>
2020-06-04 21:03:45 -0300

    adicionando estrutura basica do react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
+
+import Routes from './routes'
 
 function App() {
   return (
-        <p>Edit and save to reload.</p>
+        <div>
+          <Routes/>
+        </div>
   );
 }
 
